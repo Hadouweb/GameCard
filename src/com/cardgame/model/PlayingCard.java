17f4215a00e39e7fa0c6a933ca53ee9fa5befc16
Nicolas Le Breton <nicolas.le.breton.0xff@gmail.com>
2023-08-25 19:15:41 +0200

    Init

---
+++ src/com/cardgame/model/PlayingCard.java
@@ -0,0 +1,27 @@
+package com.cardgame.model;
+
+public class PlayingCard {
+
+    private Rank rank;
+    private Suit suit;   
+    private boolean faceUp;
+    
+    public PlayingCard(Rank rank, Suit suit) {
+	super();
+	this.rank = rank;
+	this.suit = suit;
+    }
+
+    public Rank getRank() {
+	return rank;
+    }
+    
+    public Suit getSuit() {
+	return suit;
+    }
+    
+    public boolean flip() {
+	faceUp = !faceUp;
+	return faceUp;
+    }
+}
